7163858ce65f557d6fc583a1a9372bd7472ade13
singley-sci <joel.singley@gmail.com>
2019-09-05 14:49:03 -0600

    cleaned up plots, adjusted color scales

--- assignment_1.R
+++ assignment_1.R
@@ -11,7 +11,6 @@ library(lubridate)
 #Reading in files
 files <- list.files('data',full.names=T)
 
-
 #Read in individual data files
 ndmi <- read_csv(files[1]) %>% 
   rename(burned=2,unburned=3) %>%
@@ -36,6 +35,7 @@ full_long <- rbind(ndvi,ndmi,ndsi) %>%
 #function "spread" and then make a plot that shows the correlation as a
 # function of if the site was burned or not
 
+# Convert data to wide format, remove NAs, then add month, year, and treatment columns
 full_wide <- spread(data=full_long, key='data', value='value') %>%
   filter_if(is.numeric, all_vars(!is.na(.))) %>%
   mutate(month=month(DateTime), 
@@ -43,18 +43,21 @@ full_wide <- spread(data=full_long, key='data', value='value') %>%
          treatment = cut(year,breaks=c(0,2003,2020),
                          labels=c('pre-burn','post-burn')))
 summary(full_wide) #check that all NAs removed
-view(full_wide) #
+#view(full_wide)
 
-summer_only <- full_wide %>%
+# Filter out only summer months (June-September)
+summer_wide <- full_wide %>%
   filter(month %in% c(6:9)) %>%
-  mutate(season='summer')
+  mutate(season='summer') #add column 'season' with value 'summer'
 
-summer_only %>%
+# Plot of NDVI vs NDMI colored by site
+summer_wide %>%
   ggplot(aes(x=ndmi, y=ndvi, color=site)) +
-  geom_point(alpha=0.5) +
+  geom_point(alpha=0.8) +
   theme_few() +
-  scale_color_few() +
-  theme(legend.position = c(0.8,0.8))
+  scale_color_hc() + #color scale used for sites
+  theme(legend.position = c(0.8,0.8)) +
+  xlab('NDMI') + ylab('NDVI')
 
 ## End Code for Question 1 -----------
 
@@ -65,20 +68,22 @@ summer_only %>%
 #In other words, does the previous year's snow cover influence vegetation
 # growth for the following summer? 
 
-winter_only <- full_wide %>%
+# Filter out winter only 
+winter_wide <- full_wide %>%
   filter(month %in% c(1:4)) %>%
   mutate(season='winter')
 
-summer_winter <- full_join(x=winter_only, y=summer_only) %>%
+seasons_wide <- full_join(x=winter_wide, y=summer_wide) %>%
   group_by(site,season, year) %>%
   summarize(mean_ndvi=mean(ndvi), mean_ndsi=mean(ndsi))
-view(summer_winter)
+#view(seasons_wide)
 
-summer_winter %>%
+# Plot of summer NDVI vs NDSI from prior witner
+seasons_wide %>%
   ggplot(aes(x=mean_ndsi, y=mean_ndvi, color=site)) +
-  geom_point(alpha=0.5) +
+  geom_point(alpha=0.8) +
   theme_few() +
-  scale_color_few() +
+  scale_color_hc() + #color scale used for sites
   theme(legend.position = c(0.8,0.8))
 
 ## Your code here
@@ -90,17 +95,17 @@ summer_winter %>%
 #How is the snow effect from question 2 different between pre- and post-burn
 # and burned and unburned? 
 
-summer_winter_treatment <- full_join(x=winter_only, y=summer_only) %>%
+seasons_treatment_wide <- full_join(x=winter_wide, y=summer_wide) %>%
   group_by(site,season,treatment, year) %>%
   summarize(mean_ndvi=mean(ndvi), mean_ndsi=mean(ndsi))
-view(summer_winter_treatment)
+#view(seasons_treatment_wide)
 
-summer_winter_treatment %>%
+seasons_treatment_wide %>%
   ggplot(aes(x=mean_ndsi, y=mean_ndvi, color=treatment)) +
-  geom_point(alpha=0.5) +
+  geom_point(alpha=0.8) +
   facet_wrap(~site) +
   theme_few() +
-  scale_color_few() +
+  scale_color_few() + #color scale used for site
   theme(legend.position = c(0.7,0.2))
 
 ## End code for question 3
@@ -109,27 +114,27 @@ summer_winter_treatment %>%
 #What month is the greenest month on average? Does this change in the burned
 # plots after the fire? 
 
-monthly_ndvi_ndsi <- full_wide %>%
+monthly_wide <- full_wide %>%
   group_by(site, month, treatment) %>%
-  summarize(mean_ndvi=mean(ndvi), mean_ndsi=mean(ndsi))
+  summarize(mean_ndvi=mean(ndvi), mean_ndsi=mean(ndsi), mean_ndmi=mean(ndmi))
 #view(monthly_ndvi)
 
 # Line plot of monthly average NDVI by site and treatment
-monthly_ndvi_ndsi  %>%
+monthly_wide  %>%
   ggplot(aes(x=month, y=mean_ndvi, color=treatment)) +
   geom_line() +
   facet_wrap(~site) +
   theme_few() +
-  scale_color_few() +
+  scale_color_few() + #color scale used for treatments
   theme(legend.position = c(0.75,0.2)) +
   scale_x_continuous(breaks=1:12) + #forces integers on x axis
   xlab('Month') + ylab('NDVI')
 
-# Box plot of NDVI by month and treatment faceted by site
-# For this to work, comment out line 114 and pipe on line 113
-# when generating monthly_ndvi_ndsi
+# Box plot of NDSI by month and treatment faceted by site
+# For this to work instead: comment out summarize line and prior pipe
+# when generating monthly_wide
 
-  #monthly_ndvi_ndsi  %>%
+  #monthly_wide  %>%
   #  ggplot(aes(x=factor(month), y=ndvi, color=treatment)) +
   #  geom_boxplot(outlier.size=1, outlier.alpha=0.5) +
   #  facet_wrap(~site) +
@@ -142,21 +147,21 @@ monthly_ndvi_ndsi  %>%
 ##### Question 5 ####
 #What month is the snowiest on average?
 
-monthly_ndvi_ndsi  %>%
+monthly_wide  %>%
   ggplot(aes(x=month, y=mean_ndsi, color=treatment)) +
   geom_line() +
   facet_wrap(~site) +
   theme_few() +
-  scale_color_few() +
+  scale_color_few() + #color scale used for treatments
   theme(legend.position = c(0.75,0.8)) +
   scale_x_continuous(breaks=1:12) +
   xlab('Month') + ylab('NDSI')
 
 # Box plot of NDSI by month and treatment faceted by site
-# For this to work, comment out line 114 and pipe on line 113
-# when generating monthly_ndvi_ndsi
+# For this to work instead: comment out summarize line and prior pipe
+# when generating monthly_wide
 
-#monthly_ndvi_ndsi  %>%
+#monthly_wide  %>%
 #  ggplot(aes(x=factor(month), y=ndsi, color=treatment)) +
 #  geom_boxplot(outlier.size=1, outlier.alpha=0.5) +
 #  facet_wrap(~site) +
